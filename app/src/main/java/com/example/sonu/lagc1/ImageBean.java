49e3fbb25b9195bee53991a240b290bfdde6a7df
ShrutiSoni1991 <shruti9575@gmail.com>
2018-05-06 10:31:19 -0500

    initial commit

---
+++ app/src/main/java/com/example/sonu/lagc1/ImageBean.java
@@ -0,0 +1,17 @@
+package com.example.sonu.lagc1;
+
+/**
+ * Created by Sonu on 16-04-2017.
+ */
+
+public class ImageBean {
+    public String getImgName() {
+        return imgName;
+    }
+
+    public void setImgName(String imgName) {
+        this.imgName = imgName;
+    }
+
+    private String imgName;
+}
